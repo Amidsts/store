c4ca0c0b3c6d98aad3fa78c152b365456e17a608
Amidsts <mustaphaamidatoyindamola@gmail.com>
2024-03-11 15:03:05 +0100

    unit test for sign up controller

--- src/__tests__/components/Auth/controllers/signup.spec.ts
+++ src/__tests__/components/Auth/controllers/signup.spec.ts
@@ -3,5 +3,7 @@ const request = require("supertest");
 import app from "../../../../app";
 
 describe("user signup", () => {
-  it("Should ");
+  it("Should throw error if user already exist");
+  it("should create a new user with a success message");
+  it('should handle "internal server error" for catch error');
 });
