1433ad1a6f0fa92c10be72c32b56c6d47b1ae4cf
Amidsts <mustaphaamidatoyindamola@gmail.com>
2024-03-11 15:53:02 +0100

    testing

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -9,17 +9,12 @@ async function connectMongoDb(): Promise<void> {
 
   try {
     await mongoose.connect(appConfig.mongoDbUri, options);
+    console.log("mongodb uri: ", appConfig.mongoDbUri);
     console.log("Database connected");
-
   } catch (error) {
     console.log("Error connecting to database: " + error);
     process.exit(1);
   }
-
-  // Listen for errors after the initial connection
-  mongoose.connection.on("error", (error) => {
-    console.log("Database error: " + error);
-  });
 }
 
 export default connectMongoDb
